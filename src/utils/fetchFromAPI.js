c7a2b8ede5f57e953901502ad22dfed370de986b
anakagung013 <104742549+anakagung013@users.noreply.github.com>
2024-07-29 09:22:59 +0800

    Update fetchFromAPI.js

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -5,7 +5,7 @@ const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 const options = {
   url: BASE_URL,
   params: {
-    maxResults: '20'
+    maxResults: '50'
   },
   headers: {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
@@ -13,18 +13,7 @@ const options = {
   }
 };
 
-const allowedCategories = ['Official Music Video', 'Official Music Audio'];
-
 export const fetchFromAPI = async (url) => {
   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-  const filteredData = data.items.filter(item => {
-    const title = item.snippet.title.toLowerCase();
-    const channelTitle = item.snippet.channelTitle.toLowerCase();
-    
-    return (
-      allowedCategories.some(category => title.includes(category.toLowerCase())) ||
-      channelTitle.includes('topic')
-    );
-  });
-  return filteredData;
+  return data;
 };
